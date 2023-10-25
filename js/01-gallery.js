95a261a0b82f6f60d85c9db11307b21356725a28
IhorRatsyn <ratsinjob@gmail.com>
2023-10-25 19:44:18 +0300

    323

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createGalleryItem(item) {
+  const li = document.createElement('li');
+  li.classList.add('gallery__item');
+
+  const a = document.createElement('a');
+  a.classList.add('gallery__link');
+  a.href = item.original;
+
+  const img = document.createElement('img');
+  img.classList.add('gallery__image');
+  img.src = item.preview;
+  img.dataset.source = item.original;
+  img.alt = item.description;
+
+  a.appendChild(img);
+  li.appendChild(a);
+  return li;
+}
+
+const galleryItemsHTML = galleryItems.map(createGalleryItem);
+gallery.append(...galleryItemsHTML);
+
+gallery.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (event.target.nodeName === 'IMG') {
+    const source = event.target.dataset.source;
+    const instance = basicLightbox.create(`<img src="${source}" alt="Image description">`);
+    instance.show();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createGalleryItem(item) {
+  const li = document.createElement('li');
+  li.classList.add('gallery__item');
+
+  const a = document.createElement('a');
+  a.classList.add('gallery__link');
+  a.href = item.original;
+
+  const img = document.createElement('img');
+  img.classList.add('gallery__image');
+  img.src = item.preview;
+  img.alt = item.description;
+
+  a.appendChild(img);
+  li.appendChild(a);
+  return li;
+}
+
+const galleryItemsHTML = galleryItems.map(createGalleryItem);
+gallery.append(...galleryItemsHTML);
+
+// Підключення бібліотеки SimpleLightbox
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
