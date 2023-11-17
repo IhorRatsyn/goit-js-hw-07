2274379d9136dfcea669a1884849e54aa16569b5
IhorRatsyn <ratsinjob@gmail.com>
2023-11-17 22:58:36 +0200

    11

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,9 +3,6 @@ import { galleryItems } from './gallery-items.js';
 const gallery = document.querySelector('.gallery');
 
 function createGalleryItem(item) {
-  const li = document.createElement('li');
-  li.classList.add('gallery__item');
-
   const a = document.createElement('a');
   a.classList.add('gallery__link');
   a.href = item.original;
@@ -16,8 +13,7 @@ function createGalleryItem(item) {
   img.alt = item.description;
 
   a.appendChild(img);
-  li.appendChild(a);
-  return li;
+  return a;
 }
 
 const galleryItemsHTML = galleryItems.map(createGalleryItem);
@@ -32,10 +28,4 @@ document.addEventListener('DOMContentLoaded', () => {
     captionsData: 'alt',
     captionDelay: 250,
   });
-});
-
-
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
 });
